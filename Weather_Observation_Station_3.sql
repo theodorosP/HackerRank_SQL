20c1be8ecc19f06136d9f436a5bffc1d2eb2f22a
thodorisP <thodorisP@github.com>
2023-12-09 08:48:10 -0500

    second commit

---
+++ Weather_Observation_Station_3.sql
@@ -0,0 +1,18 @@
+/*
+
+Given a table STATION that holds data for five fields namely ID, CITY, STATE, NORTHERN LATITUDE and WESTERN LONGITUDE.
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+ 
+Write a query to print the sum of LAT_N and the sum of  LONG_W separated by space, up to 2 decimal places 
+
+*/
+
+SELECT DISTINCT CITY FROM STATION WHERE ID % 2 = 0;
