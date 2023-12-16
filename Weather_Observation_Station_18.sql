88362721ca563241983972cdd6416c6dd2c0b2e2
thodorisP <teosfp@hotmail.com>
2023-12-16 07:26:01 -0500

    commit

---
+++ Weather_Observation_Station_18.sql
@@ -0,0 +1,28 @@
+/*
+
+Given a table STATION that holds data for five fields namely ID, CITY, STATE, NORTHERN LATITUDE and WESTERN LONGITUDE.
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+
+Consider P1(a, b) and P2(c, d) to be two points on a 2D plane.
+
+a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+
+Query the Manhattan Distance between points
+and and round it to a scale of 4 decimal places.
+*/
+
+SELECT ROUND(ABS((SELECT MIN(LAT_N) FROM STATION) - (SELECT MAX(LAT_N) FROM STATION)) + ABS((SELECT MIN(LONG_W) FROM STATION) - (SELECT MAX(LONG_W) FROM STATION)) , 4) AS RES
+
+--ALTERNATIVELY, YOU CAN DO THIS:
+SELECT ROUND(ABS(MIN(LAT_N) - MAX(LAT_N)) + ABS(MIN(LONG_W) - MAX(LONG_W)) , 4) FROM STATION
