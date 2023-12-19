46f3f0a100be65139cd451e9762586564d547fdd
thodorisP <teosfp@hotmail.com>
2023-12-19 03:26:14 -0500

    commit

---
+++ Japan_Population.sql
@@ -0,0 +1,20 @@
+/*
+Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN.
+
+Input Format
+
+The CITY table is described as follows:
+
++-------------+--------------+
+| Field       | Type         |
++-------------+--------------+
+| ID          | NUMBER       |
+| NAME        | VARCHAR2(17) |
+| COUNTRYCODE | VARCHAR2(3)  |
+| DISTRICT    | VARCHAR2(20) |
+| POPULATION  | NUMBER       |
++-------------+--------------+
+Query a list of CITY and STATE from the STATION table. 
+ */
+
+SELECT SUM(POPULATION) FROM CITY WHERE COUNTRYCODE = 'JPN' ;
---
+++ Population_Density_Difference.sql
@@ -0,0 +1,20 @@
+/*
+Query the difference between the maximum and minimum populations in CITY.
+
+Input Format
+
+The CITY table is described as follows:
+
++-------------+--------------+
+| Field       | Type         |
++-------------+--------------+
+| ID          | NUMBER       |
+| NAME        | VARCHAR2(17) |
+| COUNTRYCODE | VARCHAR2(3)  |
+| DISTRICT    | VARCHAR2(20) |
+| POPULATION  | NUMBER       |
++-------------+--------------+
+Query a list of CITY and STATE from the STATION table. 
+ */
+
+SELECT MAX(POPULATION) - MIN(POPULATION) FROM CITY;
