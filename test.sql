cc5561c45e389915fc1828d309bb51cc83b7d4f0
thodorisP <teosfp@hotmail.com>
2024-01-04 11:35:00 -0500

    commit

--- test.sql
+++ test.sql
@@ -11,6 +11,9 @@ CREATE TABLE People (
 
 INSERT INTO People VALUES (100, 'Theodoros', 'Pan', 'Orlando');
 INSERT INTO People VALUES (11, 'Aheodoros', 'Pan', 'Orlando');
+INSERT INTO People VALUES (100, 'Theodoros', 'Pan1', 'Orlando');
+INSERT INTO People VALUES (100, 'Theodoros', 'Pan1', 'Orlando');
+INSERT INTO People VALUES (100, 'Theodoros', 'Pan1', 'Orlando');
 INSERT INTO People VALUES (12, 'Aheodoros', 'Pan', 'Orlando');
 INSERT INTO People VALUES (9, 'Theo', 'Pn', 'Orlando');
 INSERT INTO People VALUES (50, 'T', 'Pan', 'Orlando');
@@ -24,20 +27,13 @@ INSERT INTO People VALUES (90, 'A2', 'Pan', 'Orlando');
 INSERT INTO People VALUES (190, 'A', 'Pan', 'Orlando');
 INSERT INTO People VALUES (3330, 'Theo', 'Pan', 'Orlando');
 
-DROP TABLE IF EXISTS ttt;
+select FName, CONCAT('(', left(LName, 1), ')') from People order by FName;
 
-CREATE TABLE ttt (
-    ID INT,
-    FName VARCHAR(255),
-    LName VARCHAR(255),
-    city VARCHAR(255)
-);
 
-INSERT INTO ttt VALUES (1212, 'Theodoros', 'Pan', 'Orlando');
-INSERT INTO ttt VALUES (1122, 'Aheodoros', 'Pan', 'Orlando');
-INSERT INTO ttt VALUES (12121, 'Aheodoros', 'Pan', 'Orlando');
-INSERT INTO ttt VALUES (9012, 'Theo', 'Pn', 'Orlando');
-INSERT INTO ttt VALUES (51230, 'T', 'Pan', 'Orlando');
-INSERT INTO ttt VALUES (90, 'Correct', 'Pan', 'NY');
 
-select  FName, avg(ID) from People group by FName;
+
+select concat(FName, "(", substr(LName, 1, 1), ")" ) from People order by FName;
+
+
+
+select concat("There is a total of ", count(FName), " ", lower(LName), "s." ) from People group by LName order by count(LName) ASC;
